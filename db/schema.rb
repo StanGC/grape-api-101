3ba84ab7d9ac618c2045a509cfe449beef73b4f2
stan <solve153@gmail.com>
2019-04-09 18:06:17 +0800

    Add api_access_token model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_094121) do
+ActiveRecord::Schema.define(version: 2019_04_09_100237) do
+
+  create_table "api_access_tokens", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
